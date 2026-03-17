0a1dc3c2ff50f841419f94d0a5121fbe97a42510
Mateus Silva <mateus.silva@dopster.io>
2026-03-17 15:04:23 -0300

    feat: add players in localstorage

--- components/Round.tsx
+++ components/Round.tsx
@@ -1,3 +1,18 @@
-export const Round = () => {
+export const Round = ({
+    players,
+    turnIndex,
+    secretWord,
+    impostorIds,
+    setPhase
+}) => {
+    const i = Math.floor(Math.random() * players.length);
 
+    return (
+        <>
+            <h2>
+                Quem começa é: <br />
+
+            </h2>
+        </>
+    );
 }
